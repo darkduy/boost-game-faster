bc780d5cb843aca0c499810c132b59f0a958b6b9
darkduy <hung0978682878@gmail.com>
2025-07-29 09:41:44 +0700

    Update BackgroundProcessPackage.java

--- android/app/src/main/java/com/boostgamefaster/BackgroundProcessPackage.java
+++ android/app/src/main/java/com/boostgamefaster/BackgroundProcessPackage.java
@@ -5,16 +5,13 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class BackgroundProcessPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new BackgroundProcessModule(reactContext));
-        return modules;
+        return Collections.singletonList(new BackgroundProcessModule(reactContext));
     }
 
     @Override
