83f87ceeaec459639034d596a08a5568ec5f4680
darkduy <hung0978682878@gmail.com>
2025-07-29 10:07:51 +0700

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,22 @@
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transformOptions: {
-        enableBabelRCLookup: false,
-        enableBabelRuntime: true,
-      },
-    }),
-  },
-  resolver: {
-    sourceExts: ['jsx', 'js', 'ts', 'tsx'],
-  },
-};
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      sourceExts: [...sourceExts, 'svg'],
+      assetExts: [...assetExts.filter(ext => ext !== 'svg'), 'json'],
+    },
+  };
+})();
